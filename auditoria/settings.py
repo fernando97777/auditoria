4a0e0bae959df90ad79b0fbaa9fbcd7075af2e88
luisf <fervelez977@gmail.com>
2023-03-21 14:46:53 -0500

    "second commit requirements"

--- auditoria/settings.py
+++ auditoria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$+=etj-72=-f&fo3+7!#c_u5==#)$o3_69(1)fr6arl!k4$)h1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'auditoria.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
